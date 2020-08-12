3ea5710c386c68e346a5e575c5544ea2b6443af0
Peter Simon <psimon0420@gmail.com>
2020-08-12 12:58:24 -0700

    Add Zint.jl
    Also add tests for Rings.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using Test
 using SafeTestsets
 
+@safetestset "Rings Tests" begin include("Rings_test.jl") end
 @safetestset "RWGSheet Tests" begin include("RWGSheet_test.jl") end
 @safetestset "Elements Tests" begin include("Elements_test.jl") end
 @safetestset "RWGData Tests" begin include("RWGData_test.jl") end
