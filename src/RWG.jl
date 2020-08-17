b071785a9f23d7eb47189c93382138525d583eb3
Peter Simon <psimon0420@gmail.com>
2020-08-17 09:40:22 -0700

    Rearranged comments

--- src/RWG.jl
+++ src/RWG.jl
@@ -58,11 +58,13 @@ struct RWGData
     #  are numbered in the same order. Since the periodic Green's functions are 
     #  translationally invariant, the integrals involving equivalent face pairs
     #  will have identical values and thus need be computed only a single time.
+    ufpm::Array{Int,2}
+    
     #  ufp2fp[i] contains the vector of face pair indices for equivalence class i. The 
     #  face pair index uses column major ordering to enumerate the elements 
     #  of a matrix of order Nface × Nface.
-    ufpm::Array{Int,2}
     ufp2fp::Array{Array{Int,1},1}
+
     nufp::Int  # Number of unique face pairs.
 end # mutable struct
   
